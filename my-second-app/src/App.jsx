841f0c7423cdac28f5907bbea1dacd80c3ad1a62
Yigit <yigityumus@gmail.com>
2023-09-19 15:21:22 +0300

    Third commit

--- my-second-app/src/App.jsx
+++ my-second-app/src/App.jsx
@@ -1,11 +1,11 @@
-import Starter from './tutorial/01-useState/starter/01-error-example'
+import Starter from "./tutorial/01-useState/starter/02-useState-basics";
 
 function App() {
-  return (
-    <div className='container'>
-      < Starter />
-    </div>
-  );
+	return (
+		<div className="container">
+			<Starter />
+		</div>
+	);
 }
 
 export default App;
--- my-second-app/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ my-second-app/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+	const [count, setCount] = useState(0);
+
+	const handleClick = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div>
+			<h4>You clicked {count} times</h4>
+			<button type="button" className="btn" onClick={handleClick}>
+				Increase
+			</button>
+		</div>
+	);
 };
 
 export default UseStateBasics;
