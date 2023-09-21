cbe894a1ad7c7bd8adc1d84667d6e31d77482638
Yigit <yigityumus@gmail.com>
2023-09-21 14:19:00 +0300

    conditionalRendering-commit-2

--- my-second-app/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ my-second-app/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+	// falsy variable
+	const [text, setText] = useState("");
+
+	// truthy variable
+	const [name, setName] = useState("susan");
+
+	return (
+		<div>
+			<h4>Falsy OR: {text || "hello world"}</h4>
+			<h4>Falsy AND: {text && "hello world"}</h4>
+			<h4>Truthy OR: {name || "hello world"}</h4>
+			<h4>Truthy OR: {"hello world" || name}</h4>
+			<h4>Truthy AND: {name && "hello world"}</h4>
+			<h4>Truthy AND: {"hello world" && name}</h4>
+		</div>
+	);
 };
 export default ShortCircuitOverview;
