776a7707116756123e7b2d88ab2377c03938ebcc
Yigit <yigityumus@gmail.com>
2023-09-25 17:50:09 +0300

    forms-commit-2

--- my-second-app/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ my-second-app/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,59 @@
-const UserChallenge = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input type='text' className='form-input' id='name' />
-        </div>
+import { useState } from "react";
+import { data } from "../../../data";
 
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-      {/* render users below */}
-    </div>
-  );
+const UserChallenge = () => {
+	const [name, setName] = useState("");
+	const [users, setUsers] = useState(data);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!name) return;
+		const newId = users.length + 1;
+		console.log(users);
+		const newUser = { id: newId, name };
+		const updatedUsers = [...users, newUser];
+		setUsers(updatedUsers);
+		setName("");
+	};
+
+	const removeUser = (id) => {
+		const updatedUsers = users.filter((person) => person.id !== id);
+		setUsers(updatedUsers);
+	};
+
+	return (
+		<div>
+			<form className="form" onSubmit={handleSubmit}>
+				<h4>Add User</h4>
+				<div className="form-row">
+					<label htmlFor="name" className="form-label">
+						name
+					</label>
+					<input
+						type="text"
+						className="form-input"
+						id="name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				<button type="submit" className="btn btn-block">
+					submit
+				</button>
+			</form>
+			{/* render users below */}
+			<h2>users</h2>
+			{users.map((user) => {
+				return (
+					<div key={user.id}>
+						<h4>{user.name}</h4>
+						<button className="btn" onClick={() => removeUser(user.id)}>
+							remove
+						</button>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 export default UserChallenge;
