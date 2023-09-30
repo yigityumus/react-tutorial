c53d80550ff028bdbc7e994dfa902fae4de09f4e
Yigit <yigityumus@gmail.com>
2023-09-30 09:52:06 +0300

    performance-commit-3

--- my-second-app/src/App.jsx
+++ my-second-app/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge/index";
+import Starter from "./tutorial/11-performance/starter/03-hooks/index";
 
 function App() {
 	return (
